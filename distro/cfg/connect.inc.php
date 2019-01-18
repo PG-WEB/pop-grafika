508d67f257a906108b93a7babd585d64826c5ea9
github username <github email address>
2019-01-17 23:27:52 -0500

    distro fixed

--- distro/cfg/connect.inc.php
+++ distro/cfg/connect.inc.php
@@ -2,11 +2,11 @@
 	//database connection settings
 
 	define('DB_HOST', 'localhost'); // database host
-	define('DB_USER', 'popgrafi_pg'); // username
+	define('DB_USER', 'popgrafi_pg2'); // username
 	define('DB_PASS', 'pg'); // password
-	define('DB_NAME', 'popgrafi_distro'); // database name
-	define('ADMIN_LOGIN', 'YWRtaW4='); //administrator's login
-	define('ADMIN_PASS', '81dc9bdb52d04dc20036dbd8313ed055'); //administrator's login
+	define('DB_NAME', 'popgrafi_distro2'); // database name
+	define('ADMIN_LOGIN', 'YWRtaW4='); //administrator's login admin
+	define('ADMIN_PASS', '81dc9bdb52d04dc20036dbd8313ed055'); //administrator's login 1234
 
 	//database tables
 	include("./cfg/tables.inc.php");
